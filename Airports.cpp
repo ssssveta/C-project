d9676bbf0d7182b17de89d1e81c4e31197906552
root <root@LAPTOP-TDB0OHP5>
2023-02-21 21:02:46 +0300

    Airports and Planes

--- Airports.cpp
+++ Airports.cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 #include "Planes.cpp"
 using namespace std;
+
 class Airports {
 public:
     string name;
@@ -8,7 +9,7 @@ public:
     float latitude;
     float longitude;
     bool status; // 0 - close, 1 - open
-    vector<Planes> empty_planes();
+    vector<Planes> empty_planes;
     Airports();
     Airports(string arg_name, string arg_city, float arg_latitude, float arg_longitude, bool arg_status, vector<Planes> &arg_empty_planes) {
         name = arg_name;
@@ -16,23 +17,23 @@ public:
         latitude = arg_latitude;
         longitude = arg_longitude;
         status = arg_status;
-        for (int i = 0; i < arg_empty_planes.size(); i++) empty_planes.push_back(arg_empty_planes[i])
+        for (int i = 0; i < arg_empty_planes.size(); i++) empty_planes.push_back(arg_empty_planes[i]);
     }
     void print() {
         cout << "name: " << name << '\n' << "city: " << city << '\n' << "latitude: " << latitude << '\n' << "longitude: " << longitude << '\n';
         if (status) cout << "status: " << "open" << '\n';
         else cout << "status: " << "close" << '\n';
         int num = empty_planes.size();
-        for (int i = 0; i < num; i++) cout << empty_planes[i].print;
+        for (int i = 0; i < num; i++) empty_planes[i].print();
     }
 };
 int main() {
     Planes first("Su-57");
     Planes second("Mig-29");
-    vector<Planes> vec_planes (0);
+    vector<Planes> vec_planes;
     vec_planes.push_back(first);
     vec_planes.push_back(second);
     Airports vnukovo("Vnukovo", "Moscow", 1.00, 2.000, 1, vec_planes);
-    vnukovo.print;
+    vnukovo.print();
     return 0;
 }
